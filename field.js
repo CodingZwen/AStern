51b53c5ecd4ef0e2a9a219e6dc2dff87c46325ef
Sven <kuhumwerfer@hotmail.de>
2021-10-27 14:22:40 +0200

    Add files via upload

--- field.js
+++ field.js
@@ -12,19 +12,23 @@ export default class Field
         this.field = [];
         this.obstacle = 'O';
         this.grass = 'W';
+        this.initField();
 
     }
 
-    drawTile(xpos,ypos,ctx)
+    drawTile(xpos,ypos,ctx,color)
     {
+
+        
 
         ctx.beginPath();
         ctx.lineWidth=1;
-
         ctx.strokeStyle = "#111111";
-        ctx.strokeRect(xpos,ypos,this.tilesize,this.tilesize);
+        ctx.strokeRect(xpos*this.tilesize,ypos*this.tilesize,this.tilesize,this.tilesize);
         ctx.stroke();
 
+        ctx.fillStyle=color;
+        ctx.fillRect(xpos*this.tilesize,ypos*this.tilesize,this.tilesize,this.tilesize);
 
     }
 
@@ -34,18 +38,28 @@ export default class Field
 
         for(let y = 0 ; y < this.height;y++)
         {
-            this.field[y] = new Array(this.width);
+           
 
             for(let x = 0 ; x < this.width;x++)
             {
 
-                this.drawTile(x*this.tilesize,y*this.tilesize,ctx);
-                console.log("ich versuche zu malen");
-                this.field[y][x] = this.grass;
+                var tileType = this.field[y][x];
+                switch(tileType)
+                {
+                    case 'W' :
+                    this.drawTile(x,y,ctx,'#00ff00');
+                    break;
+                    case 'O' :
+                        this.drawTile(x,y,ctx,'#000000');    
+                    break;
+                    default :
+                    this.drawTile(x,y,ctx,'#ff0000');
+                     break;
+                }
             }
         }
 
-        console.log(this.field);
+      
     }
 
     initField()
@@ -57,13 +71,13 @@ export default class Field
 
             for(let x = 0 ; x < this.width;x++)
             {
-
-                this.drawTile(x*this.tilesize,y*this.tilesize,ctx);              
+            
                 this.field[y][x] = this.grass;
             }
         }
 
         console.log(this.field);
+       
     }
 
 
